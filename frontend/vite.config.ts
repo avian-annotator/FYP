8103b01927fb983ebf4d9accbfc2407bde17f81b
suspiciouscap <dan.hong378@gmail.com>
2025-04-02 11:27:04 +1100

    fix hmr

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,10 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-            react(), 
-            tailwindcss()],
+  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true, // Because windows with wsl is an arsehole
+    },
+  },
 })
