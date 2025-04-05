5c89b8ee1039c30c5b480e6975dabd8fe28c7f95
suspiciouscap <dan.hong378@gmail.com>
2025-04-05 21:34:50 +1100

    fix: return 400 error for missing cookie

--- backend/src/main/java/com/example/avian_annotator/config/SecurityConfig.java
+++ backend/src/main/java/com/example/avian_annotator/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.avian_annotator.config;
 
 import com.example.avian_annotator.service.CustomDetailsService;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -58,15 +59,23 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/api/logout")
                         .logoutSuccessHandler((request, response, authentication) -> {
-                            response.setStatus(200);
-                            response.setContentType("application/json");
-                            response.getWriter().write("{\"status\":\"logged out\"}");
-                        }))
+                            HttpSession session = request.getSession(false);
+                            if (session == null) {
+                                response.setStatus(401);
+                                response.setContentType("application/json");
+                                response.getWriter().write("{\"error\":\"Not authorised\"}");
+                            } else {
+                                // Invalidate the session and send success message
+                                session.invalidate();
+                                response.setStatus(200);
+                                response.setContentType("application/json");
+                                response.getWriter().write("{\"status\":\"logged out\"}");
+                            }
+                        })
+                )
                 .exceptionHandling(exceptions -> exceptions
                         .authenticationEntryPoint((request, response, authException) -> {
-                            response.setStatus(401);
-                            response.setContentType("application/json");
-                            response.getWriter().write("{\"error\":\"Not authorised\"}");
+
                         }))
                 .build();
     }
