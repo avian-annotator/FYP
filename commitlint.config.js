3ed3b477cdfa0e8f9f8dd1e04e2a4681f59ac64f
suspiciouscap <dan.hong378@gmail.com>
2025-07-12 00:03:14 +1000

    chore: add more github actions

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,27 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ],
-  "rules": {
+  plugins: ["@ngx-devs/commitlint-plugin-imperative"],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
     "type-enum": [
       2,
       "always",
-      ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "style", "test"]
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "style",
+        "test",
+      ],
     ],
-    'type-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'always', ['sentence-case', 'lower-case']],
-    'subject-full-stop': [2, 'never', '.'],
-    'header-min-length': [2, 'always', 10],
-    'subject-verb-imperative': [2, 'always']
-  }
-}
+    "type-case": [2, "always", "lower-case"],
+    "subject-case": [2, "always", ["sentence-case", "lower-case"]],
+    "subject-full-stop": [2, "never", "."],
+    "header-min-length": [2, "always", 10],
+    "imperative-rule/en": [2, "always"],
+  },
+};
