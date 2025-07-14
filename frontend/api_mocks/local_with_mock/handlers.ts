bd22ceaa1f90765cf4724aad71309d86ceaa6fd3
suspiciouscap <dan.hong378@gmail.com>
2025-07-14 23:59:35 +1000

    test: fix tests

--- frontend/api_mocks/local_with_mock/handlers.ts
+++ frontend/api_mocks/local_with_mock/handlers.ts
@@ -1,19 +1,21 @@
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 /* eslint-disable */
 // @ts-nocheck
 import { http, HttpResponse } from 'msw'
 
 // @ts-ignore
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
-// Handlers for browser-integration mocking of API requests. Backend is running as usual, with the local profile
+
 export const handlers = [
-  // DO NOT INCLUDE login/logout mocks here, as cookies can't be set in the browser mock, and therefore won't work properly
-  // Functionally, that means that the user is always logged in. You can't log out.
-  /*  http.get(`${backendUrl}/api/auth/current_user`, async () => {
-     await delay(2000)
-     return HttpResponse.json({
-       "authenticated": true,
-       "user": "asdfasdf",
-       "role": "ROLE_ADMIN"
-     })
-   }), */
+  http.get(`${backendUrl}/api/auth/current_user`, async () => {
+    await delay(2000);
+    return HttpResponse.json({
+      "authenticated": true,
+      "user": "asdfasdf",
+      "role": "ROLE_ADMIN"
+    });
+  }),
 ]
