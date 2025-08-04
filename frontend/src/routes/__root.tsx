a8bc81c209c8488f60b7c5a0c58955e697bb2843
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-08-04 23:42:20 +1000

    feat(AA-5): added navigation menu

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,14 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { useAuth, useLogout } from '../auth'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 export const Route = createRootRoute({
   component: function BaseLayout() {
@@ -8,24 +16,53 @@ export const Route = createRootRoute({
     const mutation = useLogout()
 
     return (
-      <div className="flex flex-col w-screen h-screen">
-        <div className="h-6 w-full">
-          Probably some sort of menu bar for logging in, logging out + nav links
-          {isAuthenticated ? (
-            <button
-              type="button"
-              className="px-4 py-2 bg-blue-500 text-white rounded"
-              onClick={(): void => {
-                mutation.mutate()
-              }}
-            >
-              Log out
-            </button>
-          ) : (
-            <Link className="px-4 py-2 bg-blue-500 text-white rounded" to="/login">
-              Log in
-            </Link>
-          )}
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="absolute top-1 left-1 m-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="grid gap-2">
+                Menu
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent side="bottom" align="start">
+              {isAuthenticated ? (
+                <>
+                  <DropdownMenuItem asChild>
+                    <Link to="/workspaces" className="">
+                      Workspaces
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <Link to="/me" className="">
+                      Profile
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <button
+                      type="button"
+                      className="w-full bg-blue-500 text-white rounded"
+                      onClick={(): void => {
+                        mutation.mutate()
+                      }}
+                    >
+                      Log out
+                    </button>
+                  </DropdownMenuItem>
+                </>
+              ) : (
+                <DropdownMenuItem asChild>
+                  <Link
+                    to="/login"
+                    className="w-full block px-4 py-2 bg-blue-500 text-white rounded"
+                  >
+                    Log in
+                  </Link>
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
         <div className="flex w-full h-full">
           <Outlet />
