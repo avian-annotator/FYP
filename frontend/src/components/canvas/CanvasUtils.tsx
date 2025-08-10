d57d8aeed722b9f378fae5c67df001b7d5528ccd
Anthony Lew <ant87518@gmail.com>
2025-08-10 15:58:24 +1000

    refactor(AA-29): make bounding box use konva

--- frontend/src/components/canvas/CanvasUtils.tsx
+++ frontend/src/components/canvas/CanvasUtils.tsx
@@ -1,31 +1,6 @@
-import { JSX } from 'react'
-
 const canvasColour = Array.from({ length: 100 }, (_, i) => `hsl(${String(i * 137.5)}deg 70% 60%)`)
-type CanvasColour = (typeof canvasColour)[number]
 
-interface Coord {
-  x: number
-  y: number
-}
+const getColor = (i: number) => canvasColour[i % canvasColour.length]
+const getBackgroundColor = (i: number) => getColor(i).slice(0, -4) + '80% / 0.2)'
 
-interface CanvasObject {
-  getCanvasObjectElement: () => JSX.Element
-}
-
-const isCanvasObject = (obj: unknown): obj is CanvasObject => {
-  return typeof obj === 'object' && obj !== null && 'getCanvasObjectElement' in obj
-}
-
-interface CanvasToolMarker {
-  markerId: number
-}
-
-const isCanvasToolMarker = (obj: unknown): obj is CanvasToolMarker => {
-  return typeof obj === 'object' && obj !== null && 'markerId' in obj
-}
-
-type canvasObjects = CanvasToolMarker | CanvasObject | null
-
-export { canvasColour, isCanvasObject, isCanvasToolMarker }
-
-export type { CanvasColour, CanvasObject, canvasObjects, CanvasToolMarker, Coord }
+export { getColor, getBackgroundColor }
