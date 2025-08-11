24b2c1b82cb9941f51d27252258cac03ebb1cdc3
windneverstops <dan.hong378@gmail.com>
2025-08-12 00:27:42 +1000

    refactor(AA-30): remove validation on response dtos as per convention

--- backend/src/main/java/com/fyp/avian_annotator/dto/response/AccessibleWorkspaceResponseDTO.java
+++ backend/src/main/java/com/fyp/avian_annotator/dto/response/AccessibleWorkspaceResponseDTO.java
@@ -1,16 +1,12 @@
 package com.fyp.avian_annotator.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class AccessibleWorkspaceResponseDTO {
-  @NotNull private Long id;
-
-  @NotBlank private String name;
-
-  @NotBlank private String ownerUsername;
+  private Long id;
+  private String name;
+  private String ownerUsername;
 }
--- backend/src/main/java/com/fyp/avian_annotator/dto/response/WorkspaceResponseDTO.java
+++ backend/src/main/java/com/fyp/avian_annotator/dto/response/WorkspaceResponseDTO.java
@@ -1,14 +1,12 @@
 package com.fyp.avian_annotator.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class WorkspaceResponseDTO {
-  @NotNull private Long id;
+  private Long id;
 
-  @NotBlank private String name;
+  private String name;
 }
