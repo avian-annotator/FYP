b3f025aaa4f0a751cd3fb90a122643da1959388b
windneverstops <dan.hong378@gmail.com>
2025-08-14 22:27:52 +1000

    fix(AAS-10): make all validation use notempty over notblank

--- backend/src/main/java/com/fyp/avian_annotator/dto/response/AccessibleWorkspaceResponseDTO.java
+++ backend/src/main/java/com/fyp/avian_annotator/dto/response/AccessibleWorkspaceResponseDTO.java
@@ -1,7 +1,7 @@
 package com.fyp.avian_annotator.dto.response;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record AccessibleWorkspaceResponseDTO(
-    @NotNull Long id, @NotNull String name, @NotBlank String ownerUsername) {}
+    @NotNull Long id, @NotEmpty String name, @NotEmpty String ownerUsername) {}
