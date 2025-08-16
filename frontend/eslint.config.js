d52d60504a3a8df12b1c53a3d8757e8c0fd92d87
windneverstops <dan.hong378@gmail.com>
2025-08-16 19:56:15 +1000

    chore(AAS-0): update ts configs and generation

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -17,7 +17,7 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        project: ['./tsconfig.node.json', './tsconfig.app.json', './tsconfig.generate.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
