5ba05d853ec89a12e1f8caa27aef4c619be5373c
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-08-22 13:35:18 +1000

    feat(AAS-15): added tool select sidebar

--- frontend/src/pages/AnnotateWorkspace.tsx
+++ frontend/src/pages/AnnotateWorkspace.tsx
@@ -1,5 +1,7 @@
 import { useParams } from '@tanstack/react-router'
 import { Route } from '../routes/workspaces/$workspaceId/annotate/$imageId'
+import { ToolSelectorSidebar, ToolId } from '@/components/workspace/ToolSelectorSidebar'
+import { useState } from 'react'
 
 type AnnotateWorkspaceParams = {
   workspaceId: string
@@ -9,12 +11,23 @@ type AnnotateWorkspaceParams = {
 
 export function AnnotateWorkspace() {
   const params: AnnotateWorkspaceParams = useParams({ from: Route.id })
-
+  const [active, setActive] = useState<ToolId>('select')
   return (
-    <div>
-      Annotate Workspace: {params.workspaceId}, Image: {params.imageId}{' '}
+    <div className="h-screen w-screen">
+      <div className="flex justify-end pt-2 p-1">
+        <ToolSelectorSidebar active={active} onSelect={setActive} />
+      </div>
+      <div className="flex items-center justify-center flex-col gap-1">
+        {' '}
+        <p>
+          {' '}
+          Workspace: {params.workspaceId}, Image: {params.imageId}
+        </p>
+        <p className="text-lg">
+          Currently selected: <span className="font-semibold">{active}</span>
+        </p>
+      </div>
     </div>
   )
-  //TODO: create annotation page
 }
 export default AnnotateWorkspace
