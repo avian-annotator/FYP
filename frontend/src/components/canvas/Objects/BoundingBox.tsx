782d7e9e12dcfcc748a25f0812a645560264e1be
Anthony Lew <ant87518@gmail.com>
2025-09-03 22:25:47 +1000

    fix(AAS-6): fix prettier formatting

--- frontend/src/components/canvas/Objects/BoundingBox.tsx
+++ frontend/src/components/canvas/Objects/BoundingBox.tsx
@@ -4,55 +4,53 @@ import { RefObject, useState } from 'react'
 import { CanvasObjectProps } from '../Canvas'
 import { getBackgroundColor } from '../CanvasUtils'
 
-
-
-interface BoundingBoxProps extends CanvasObjectProps{
-    initialPos: {x: number, y: number}
-    ref: RefObject<null | Konva.Rect>
-    label?: string
-    color?: string
+interface BoundingBoxProps extends CanvasObjectProps {
+  initialPos: { x: number; y: number }
+  ref: RefObject<null | Konva.Rect>
+  label?: string
+  color?: string
 }
 
 const BoundingBox = (props: BoundingBoxProps) => {
-    const [pos, setPos] = useState<{x:number, y:number}>(props.initialPos)
-    const color = props.color || "#ffffff"
-    return (
-        <>
-            <Rect
-                x={pos.x}
-                y={pos.y}
-                width={0}
-                height={0}
-                ref={props.ref}
-                stroke={color}
-                strokeWidth={2}
-                fill={getBackgroundColor(props.id)}
-                key={props.id}
-                id={`stage.${String(props.id)}`}
-                onDragMove={e=>{
-                    setPos({x:e.target.x(),y:e.target.y()})
-                }}
-                onTransform={e=>{
-                    setPos({x:e.target.x(),y:e.target.y()})
-                }}
-                onMouseUp={e=>{
-                    setPos({x:e.target.x(),y:e.target.y()})
-                }}
-            />
-            {props.label && (
-                <Text
-                    x={pos.x}
-                    y={pos.y-25}
-                    text={props.label}
-                    fontSize={25}
-                    fontFamily="Arial"
-                    fill={color}
-                    key={`label.${String(props.id)}`}
-                    id={`label.${String(props.id)}`}
-                />
-            )}
-        </>
-    )
+  const [pos, setPos] = useState<{ x: number; y: number }>(props.initialPos)
+  const color = props.color || '#ffffff'
+  return (
+    <>
+      <Rect
+        x={pos.x}
+        y={pos.y}
+        width={0}
+        height={0}
+        ref={props.ref}
+        stroke={color}
+        strokeWidth={2}
+        fill={getBackgroundColor(props.id)}
+        key={props.id}
+        id={`stage.${String(props.id)}`}
+        onDragMove={e => {
+          setPos({ x: e.target.x(), y: e.target.y() })
+        }}
+        onTransform={e => {
+          setPos({ x: e.target.x(), y: e.target.y() })
+        }}
+        onMouseUp={e => {
+          setPos({ x: e.target.x(), y: e.target.y() })
+        }}
+      />
+      {props.label && (
+        <Text
+          x={pos.x}
+          y={pos.y - 25}
+          text={props.label}
+          fontSize={25}
+          fontFamily="Arial"
+          fill={color}
+          key={`label.${String(props.id)}`}
+          id={`label.${String(props.id)}`}
+        />
+      )}
+    </>
+  )
 }
 
 export default BoundingBox
