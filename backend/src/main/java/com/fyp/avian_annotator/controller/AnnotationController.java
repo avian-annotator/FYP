7bbe092775f737d0d8ce1e95364d0087288a0cc7
windneverstops <dan.hong378@gmail.com>
2025-09-15 23:06:46 +1000

    feat(AAS-23): connect relay to frontend
    # Conflicts:
    #	frontend/src/pages/AnnotateWorkspace.tsx

--- backend/src/main/java/com/fyp/avian_annotator/controller/AnnotationController.java
+++ backend/src/main/java/com/fyp/avian_annotator/controller/AnnotationController.java
@@ -4,16 +4,21 @@ import com.fyp.avian_annotator.dto.websocket.AnnotationActionPayload;
 import com.fyp.avian_annotator.dto.websocket.AnnotationPresencePayload;
 import com.fyp.avian_annotator.security.CustomUserDetails;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class AnnotationController {
+  private final SimpMessagingTemplate messagingTemplate;
+
   @MessageMapping("/workspace/{workspaceId}/image/{imageId}/annotate")
-  @SendTo("/topic/workspace/{workspaceId}/image/{imageId}/annotations")
+  @SendTo("/topic/workspace/{workspaceId}/image/{imageId}/annotate")
   public AnnotationActionPayload annotate(
       @DestinationVariable String workspaceId,
       @DestinationVariable String imageId,
--- frontend/src/pages/AnnotateWorkspace.tsx
+++ frontend/src/pages/AnnotateWorkspace.tsx
@@ -4,20 +4,63 @@ import { ToolSelectorSidebar } from '@/components/workspace/ToolSelectorSidebar'
 import { useState } from 'react'
 import { useGeneratePresignedDownloadUrlForImage } from '../../generated'
 import { Canvas } from '@/components/canvas'
+import { useEffect, useRef } from 'react'
+import { RxStomp, RxStompConfig } from '@stomp/rx-stomp'
 
 type AnnotateWorkspaceParams = {
   workspaceId: number
   imageId: string
+interface AnnotatePayload {
+  // THIS is just a placeholder for now
+  annotationAction: string
+  userId: number
+  objectId: string
+  objectType: string
 }
 
 export function AnnotateWorkspace() {
-  const params: AnnotateWorkspaceParams = useParams({ from: Route.id })
+  const { workspaceId, imageId } = useParams({ from: Route.id })
   const [active, setActive] = useState<number>(0)
 
-  const { data } = useGeneratePresignedDownloadUrlForImage(params.workspaceId, params.imageId, {
+  const { data } = useGeneratePresignedDownloadUrlForImage(workspaceId, imageId, {
     includeAnnotations: true,
   })
   const image = data?.data
+  const { workspaceId, imageId } = useParams({ from: Route.id })
+
+  const rxStompRef = useRef(new RxStomp())
+  const rxStomp = rxStompRef.current
+
+  const publish = (message: AnnotatePayload) => {
+    rxStomp.publish({
+      destination: `/app/workspace/${workspaceId}/image/${imageId}/annotate`,
+      body: JSON.stringify(message),
+    })
+  }
+
+  useEffect(() => {
+    const rxStompConfig: RxStompConfig = {
+      brokerURL: `${import.meta.env.VITE_WEBSOCKET_URL as string}/ws`,
+      heartbeatIncoming: 0,
+      heartbeatOutgoing: 0,
+      reconnectDelay: 5000,
+    }
+    rxStomp.configure(rxStompConfig)
+    rxStomp.activate()
+
+    publish({ annotationAction: 'JOIN', userId: 1, objectId: '1', objectType: 'IMAGE' })
+
+    const topic$ = rxStomp
+      .watch(`/topic/workspace/${workspaceId}/image/${imageId}/annotate`)
+      .subscribe(msg => {})
+
+    return () => {
+      void rxStomp.deactivate()
+      topic$.unsubscribe()
+    }
+
+    // Runs only once on mount, so don't need any dep arrays. They're also static anyways
+  }, [])
 
   return (
     <div className="flex h-screen items-center justify-center">
