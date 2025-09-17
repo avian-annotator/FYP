2a481ce0ece43b09fafa437fa8bf26dee1b3a9e7
Sacha Acland <83637951+seba0205@users.noreply.github.com>
2025-09-17 10:42:46 +1000

    feat(AAS-29): create keypoints tool (#46)
    * feat(AAS-25): begin drafting state machine
    
    * fix(AAS-25): remove stray recoil reference
    
    * feat(AAS-25): all tools working with new state machine
    
    * feat(AAS-29): added keypoint skeletal tool initial
    
    * feat(AAS-29): removed draggable (temp)
    
    * feat(AAS-29): added draggability for keypoints
    
    * feat(AAS-29): cleaned up dragging and added labelling
    
    * feat(AAS-25): begin drafting state machine
    
    * feat(AAS-25): all tools working with new state machine
    
    * feat(AAS-29): tidy up
    
    * feat(AAS-29): cleanup
    
    * feat(AAS-29): PR fixes & cleanup
    
    ---------
    
    Co-authored-by: Anthony Lew <anthony@v01dbreaker.cc>

--- frontend/src/components/canvas/Canvas.tsx
+++ frontend/src/components/canvas/Canvas.tsx
@@ -4,6 +4,7 @@ import { Stage, Layer, Transformer } from 'react-konva'
 import BoundingBoxTool from './Tools/BoundingBoxTool'
 import SelectMoveTool from './Tools/SelectMoveTool'
 import LabelTool from './Tools/LabelTool'
+import KeypointsTool from './Tools/KeypointsTool'
 import CanvasState, { canvasReducer, initalCanvasState, CanvasAction } from './CanvasState'
 
 interface CanvasTool {
@@ -42,7 +43,16 @@ const Canvas = ({ image, tool }: CanvasProps) => {
     const currentSelection = canvasState.canvasElements.find(el => el.props.id === selectionId)
     const canvasShape = currentSelection?.props.ref.current
     if (canvasShape !== undefined && canvasShape instanceof Konva.Shape) {
-      trRef.current?.nodes([canvasShape])
+      if (canvasShape instanceof Konva.Circle) {
+        //so keypoints are not resized
+        trRef.current?.resizeEnabled(false)
+        trRef.current?.nodes([canvasShape])
+      } else if (canvasShape instanceof Konva.Line) {
+        trRef.current?.resizeEnabled(false)
+      } else {
+        trRef.current?.nodes([canvasShape])
+        trRef.current?.resizeEnabled(true)
+      }
     } else {
       trRef.current?.nodes([])
       // make undraggable if selected
@@ -55,6 +65,7 @@ const Canvas = ({ image, tool }: CanvasProps) => {
     BoundingBoxTool({ stageRef, canvasState, canvasDispatch }),
     SelectMoveTool({ stageRef, canvasState, canvasDispatch }),
     LabelTool({ stageRef, canvasState, canvasDispatch }),
+    KeypointsTool({ stageRef, canvasState, canvasDispatch }),
   ]
   const activeTool = tools[tool] ?? tools[0]
 
