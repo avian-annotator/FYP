0edf6e6e356ccd25369788012a9207e8a8ec798c
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-09-30 23:43:40 +1000

    feat(AAS-0): yjs doc integration

--- frontend/src/components/canvas/CanvasState.ts
+++ frontend/src/components/canvas/CanvasState.ts
@@ -2,11 +2,6 @@ import Konva from 'konva'
 import { RefObject } from 'react'
 import * as Y from 'yjs'
 
-type CanvasState = {
-  userState: UserState[]
-  canvasElements: CanvasElement[]
-}
-
 type UserState = {
   userId: number
   currentSelectionId?: number
@@ -35,10 +30,27 @@ type Position = {
   y: number
 }
 
-const initalCanvasState = {
-  userState: [{ userId: 0, isDragging: false }],
-  canvasElements: [],
-} as CanvasState
+export interface CanvasState {
+  userState: Y.Array<UserState>
+  canvasElements: Y.Array<CanvasElement>
+}
+
+export function createCanvasState(ydoc: Y.Doc): CanvasState {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const map = ydoc.getMap<Y.Array<any>>('canvasState')
+
+  if (!map.has('userState')) {
+    map.set('userState', new Y.Array<UserState>())
+  }
+  if (!map.has('canvasElements')) {
+    map.set('canvasElements', new Y.Array<CanvasElement>())
+  }
+
+  return {
+    userState: map.get('userState') as Y.Array<UserState>,
+    canvasElements: map.get('canvasElements') as Y.Array<CanvasElement>,
+  }
+}
 
 type CanvasAction =
   | { type: 'addElement'; element: CanvasElement }
@@ -54,6 +66,93 @@ type CanvasAction =
   | { type: 'join' }
   | { type: 'leave' }
 
+export function yjsDispatch(canvasState: CanvasState, action: CanvasAction) {
+  const canvasElements = canvasState.canvasElements
+  const userState = canvasState.userState
+
+  switch (action.type) {
+    case 'addElement':
+      canvasElements.push([action.element])
+      break
+
+    case 'updateElement': {
+      const index = canvasElements.toArray().findIndex(el => el.id === action.id)
+      if (index >= 0) {
+        const element = {
+          ...canvasElements.get(index),
+          props: { ...canvasElements.get(index).props, ...action.props },
+        }
+        canvasElements.delete(index, 1)
+        canvasElements.insert(index, [element])
+      }
+      break
+    }
+
+    case 'removeElement': {
+      const index = canvasElements.toArray().findIndex(el => el.id === action.id)
+      if (index >= 0) canvasElements.delete(index, 1)
+      break
+    }
+
+    case 'addLabel': {
+      const index = canvasElements.toArray().findIndex(el => el.id === action.id)
+      if (index >= 0) {
+        const element = { ...canvasElements.get(index), label: action.label }
+        canvasElements.delete(index, 1)
+        canvasElements.insert(index, [element])
+      }
+      break
+    }
+
+    case 'setSelected': {
+      const index = userState.toArray().findIndex(u => u.userId === action.userId)
+      if (index >= 0) {
+        const user = { ...userState.get(index), currentSelectionId: action.id }
+        userState.delete(index, 1)
+        userState.insert(index, [user])
+      } else {
+        userState.push([
+          { userId: action.userId, currentSelectionId: action.id, isDragging: false },
+        ])
+      }
+      break
+    }
+
+    case 'clearSelected': {
+      const index = userState.toArray().findIndex(u => u.userId === action.userId)
+      if (index >= 0) {
+        const user = { ...userState.get(index), currentSelectionId: undefined }
+        userState.delete(index, 1)
+        userState.insert(index, [user])
+      } else {
+        userState.push([
+          { userId: action.userId, currentSelectionId: undefined, isDragging: false },
+        ])
+      }
+      break
+    }
+
+    case 'setDragging': {
+      const index = userState.toArray().findIndex(u => u.userId === action.userId)
+      if (index >= 0) {
+        const user = { ...userState.get(index), isDragging: action.isDragging }
+        userState.delete(index, 1)
+        userState.insert(index, [user])
+      } else {
+        userState.push([{ userId: action.userId, isDragging: action.isDragging }])
+      }
+      break
+    }
+
+    case 'addUser': {
+      const exists = userState.toArray().some(u => u.userId === action.userId)
+      if (!exists) userState.push([{ userId: action.userId, isDragging: false }])
+      break
+    }
+  }
+}
+
+/*
 function canvasReducer(p: CanvasState, action: CanvasAction) {
   // Don't publish if this is coming from websocket (handled in Canvas.tsx)
   switch (action.type) {
@@ -131,8 +230,8 @@ function canvasReducer(p: CanvasState, action: CanvasAction) {
       default:
       throw new Error()
   }
-}
+}*/
 
 export default CanvasState
 export type { UserState, CanvasElement, Position, CanvasAction, CanvasElementProps }
-export { initalCanvasState, canvasReducer }
+export {}
