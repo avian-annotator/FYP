88adc078d30686c221989025c782fd4a78d3d5de
windneverstops <dan.hong378@gmail.com>
2025-09-30 18:41:29 +1000

    chore(AAS-0): cleanup

--- backend/src/test/java/com/fyp/avian_annotator/AvianAnnotatorApplicationTests.java
+++ backend/src/test/java/com/fyp/avian_annotator/AvianAnnotatorApplicationTests.java
@@ -2,8 +2,10 @@ package com.fyp.avian_annotator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AvianAnnotatorApplicationTests {
 
   @Test
