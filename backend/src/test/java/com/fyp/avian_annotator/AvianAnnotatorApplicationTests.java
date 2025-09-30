f7089f23016c589c9b970e9ac7b4d5fbd646a59e
windneverstops <dan.hong378@gmail.com>
2025-09-30 18:59:24 +1000

    chore(AAS-0): cleanup

--- backend/src/test/java/com/fyp/avian_annotator/AvianAnnotatorApplicationTests.java
+++ backend/src/test/java/com/fyp/avian_annotator/AvianAnnotatorApplicationTests.java
@@ -1,11 +1,22 @@
 package com.fyp.avian_annotator;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest
 class AvianAnnotatorApplicationTests {
-
   @Test
   void contextLoads() {}
+
+  @TestConfiguration
+  static class MockConfig {
+    @Bean
+    public WebClient sideCarWebClient() {
+      return Mockito.mock(WebClient.class);
+    }
+  }
 }
