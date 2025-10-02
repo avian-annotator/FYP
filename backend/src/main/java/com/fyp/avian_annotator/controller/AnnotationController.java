cc83dc1d09ca6e328579574149ad8d5deed5d3bd
windneverstops <dan.hong378@gmail.com>
2025-10-02 22:01:40 +1000

    feat(AAS-0): add persistence to yjs

--- backend/src/main/java/com/fyp/avian_annotator/controller/AnnotationController.java
+++ backend/src/main/java/com/fyp/avian_annotator/controller/AnnotationController.java
@@ -4,12 +4,6 @@ import com.fyp.avian_annotator.dto.websocket.AnnotationActionPayload;
 import com.fyp.avian_annotator.dto.websocket.AnnotationPresencePayload;
 import com.fyp.avian_annotator.security.CustomUserDetails;
 import jakarta.validation.Valid;
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -22,88 +16,95 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class AnnotationController {
-  private final Map<String, List<Long>> activeSessions = new ConcurrentHashMap<>();
-  private final Environment environment;
+    private final Map<String, List<Long>> activeSessions = new ConcurrentHashMap<>();
+    private final Environment environment;
 
-  @MessageMapping("/workspace/{workspaceId}/image/{imageId}/annotate")
-  @SendTo("/topic/workspace/{workspaceId}/image/{imageId}/annotate")
-  public AnnotationActionPayload annotate(
-      @DestinationVariable String workspaceId,
-      @DestinationVariable String imageId,
-      @Valid AnnotationActionPayload message,
-      Principal principal) {
+    @MessageMapping("/workspace/{workspaceId}/image/{imageId}/annotate")
+    @SendTo("/topic/workspace/{workspaceId}/image/{imageId}/annotate")
+    public AnnotationActionPayload annotate(
+            @DestinationVariable String workspaceId,
+            @DestinationVariable String imageId,
+            @Valid AnnotationActionPayload message,
+            Principal principal) {
 
-    Long userId =
-        ((CustomUserDetails) ((UsernamePasswordAuthenticationToken) principal).getPrincipal())
-            .getId();
+        Long userId =
+                ((CustomUserDetails) ((UsernamePasswordAuthenticationToken) principal).getPrincipal())
+                        .getId();
 
-    // TODO: validation, in particular ensuring workspaceId and imageId are valid. Cache this.
-    if (Objects.equals(message.actionType(), "join")) {
-      List<Long> users =
-          activeSessions.getOrDefault(workspaceId + "/" + imageId, new ArrayList<>());
+        // TODO: validation, in particular ensuring workspaceId and imageId are valid. Cache this.
+        if (Objects.equals(message.actionType(), "join")) {
+            List<Long> users =
+                    activeSessions.getOrDefault(imageId, new ArrayList<>());
 
-      if (users.isEmpty()) {
-        webClient()
-            .post()
-            .uri(
-                uriBuilder ->
-                    uriBuilder
-                        .path("/session/workspace/{workspaceId}/image/{imageId}")
-                        .build(workspaceId, imageId))
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
-      }
+            if (users.isEmpty()) {
+                webClient()
+                        .post()
+                        .uri(
+                                uriBuilder ->
+                                        uriBuilder
+                                                .path("/session/workspace/{workspaceId}/image/{imageId}")
+                                                .build(workspaceId, imageId))
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .block();
+            }
 
-      if (!users.contains(userId)) {
-        users.add(userId);
-      }
-      activeSessions.put(workspaceId + "/" + imageId, users);
+            if (!users.contains(userId)) {
+                users.add(userId);
+            }
+            activeSessions.put(imageId, users);
 
-    } else if (Objects.equals(message.actionType(), "leave")) {
-      var users = activeSessions.getOrDefault(workspaceId + "/" + imageId, new ArrayList<>());
+        } else if (Objects.equals(message.actionType(), "leave")) {
+            var users = activeSessions.getOrDefault(imageId, new ArrayList<>());
 
-      // Check needed as react runs cleanup, so it will always send a leave at the initial mount
-      if (users.contains(userId)) {
-        users.remove(userId);
-        if (users.isEmpty()) {
-          webClient()
-              .delete()
-              .uri(
-                  uriBuilder ->
-                      uriBuilder
-                          .path("/session/workspace/{workspaceId}/image/{imageId}")
-                          .build(workspaceId, imageId))
-              .retrieve()
-              .bodyToMono(String.class)
-              .block();
+            // Check needed as react runs cleanup, so it will always send and leave at the initial mount
+            if (users.contains(userId)) {
+                users.remove(userId);
+                if (users.isEmpty()) {
+                    webClient()
+                            .delete()
+                            .uri(
+                                    uriBuilder ->
+                                            uriBuilder
+                                                    .path("/session/workspace/{workspaceId}/image/{imageId}")
+                                                    .build(workspaceId, imageId))
+                            .retrieve()
+                            .bodyToMono(String.class)
+                            .block();
+                }
+            }
+
+            activeSessions.put(imageId, users);
         }
-      }
-
-      activeSessions.put(workspaceId + "/" + imageId, users);
+        return message;
     }
-    return message;
-  }
 
-  @MessageMapping("/workspace/{workspaceId}/image/{imageId}/presence")
-  @SendTo("/topic/workspace/{workspaceId}/image/{imageId}/presence")
-  public AnnotationPresencePayload presence(
-      @DestinationVariable String workspaceId,
-      @DestinationVariable String imageId,
-      @Valid AnnotationPresencePayload message,
-      @AuthenticationPrincipal CustomUserDetails userDetails) {
+    @MessageMapping("/workspace/{workspaceId}/image/{imageId}/presence")
+    @SendTo("/topic/workspace/{workspaceId}/image/{imageId}/presence")
+    public AnnotationPresencePayload presence(
+            @DestinationVariable String workspaceId,
+            @DestinationVariable String imageId,
+            @Valid AnnotationPresencePayload message,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
 
-    return message;
-  }
+        return message;
+    }
 
-  @Bean(name = "sideCarWebClient")
-  private WebClient webClient() {
-    return WebClient.builder()
-        .baseUrl(Objects.requireNonNull(environment.getProperty("SIDECAR_URL")))
-        .build();
-  }
+    @Bean(name = "sideCarWebClient")
+    private WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(Objects.requireNonNull(environment.getProperty("SIDECAR_URL")))
+                .build();
+    }
 }
--- backend/src/main/java/com/fyp/avian_annotator/service/impl/ImageServiceImpl.java
+++ backend/src/main/java/com/fyp/avian_annotator/service/impl/ImageServiceImpl.java
@@ -12,156 +12,155 @@ import com.fyp.avian_annotator.exception.WorkspaceNotFoundException;
 import com.fyp.avian_annotator.service.ImageService;
 import com.fyp.avian_annotator.service.S3Service;
 import jakarta.transaction.Transactional;
-import java.io.InputStream;
-import java.util.Map;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
 
-  private final S3Service s3Service;
-  private final WorkspaceRepository workspaceRepository;
-  private final ImageRepository imageRepository;
+    private final S3Service s3Service;
+    private final WorkspaceRepository workspaceRepository;
+    private final ImageRepository imageRepository;
 
-  @Transactional
-  @Override
-  public String uploadImage(
-      Long userId,
-      Long workspaceId,
-      String contentType,
-      String fileName,
-      InputStream inputStream,
-      Long size) {
-    var workspace =
-        workspaceRepository
-            .findByIdAndOwnerId(workspaceId, userId)
-            .orElseThrow(() -> new WorkspaceNotFoundException(workspaceId));
+    @Transactional
+    @Override
+    public String uploadImage(
+            Long userId,
+            Long workspaceId,
+            String contentType,
+            String fileName,
+            InputStream inputStream,
+            Long size) {
+        var workspace =
+                workspaceRepository
+                        .findByIdAndOwnerId(workspaceId, userId)
+                        .orElseThrow(() -> new WorkspaceNotFoundException(workspaceId));
 
-    Image image = Image.builder().fileName(fileName).workspace(workspace).build();
-    String bucketKey = workspace.getBucketPrefix() + image.getBucketIdentifier();
+        Image image = Image.builder().fileName(fileName).workspace(workspace).build();
+        String bucketKey = workspace.getBucketPrefix() + image.getBucketIdentifier();
 
-    var metaData = Map.of("file-name", fileName);
+        var metaData = Map.of("file-name", fileName);
 
-    s3Service.uploadFile(bucketKey, contentType, metaData, inputStream, size);
+        s3Service.uploadFile(bucketKey, contentType, metaData, inputStream, size);
 
-    imageRepository.save(image);
+        imageRepository.save(image);
 
-    return image.getBucketIdentifier();
-  }
-
-  @Transactional
-  @Override
-  public EditImageDetailsResponseDTO editImageDetails(
-      Long userId, Long workspaceId, String fileName, String imageId) {
-
-    Image image = getImageIfOwned(userId, workspaceId, imageId);
-
-    if (fileName != null) {
-      image.setFileName(fileName);
-      imageRepository.save(image);
+        return image.getBucketIdentifier();
     }
-    return new EditImageDetailsResponseDTO(fileName);
-  }
 
-  @Override
-  public ImageResponseDTO createDownloadPresignedUrlForImage(
-      Long userId, Long workspaceId, String bucketIdentifier, Boolean includeAnnotations) {
+    @Transactional
+    @Override
+    public EditImageDetailsResponseDTO editImageDetails(
+            Long userId, Long workspaceId, String fileName, String imageId) {
 
-    var image = getImageIfWorkspaceUser(userId, workspaceId, bucketIdentifier);
+        Image image = getImageIfOwned(userId, workspaceId, imageId);
 
-    // not n + 1...right?
-    var url =
-        s3Service.createGetPresignedUrl(image.getWorkspace().getBucketPrefix() + bucketIdentifier);
-
-    if (includeAnnotations != null && includeAnnotations) {
-      return new ImageResponseDTO(
-          url,
-          workspaceId,
-          image.getFileName(),
-          image.getBucketIdentifier(),
-          image.getAnnotations());
+        if (fileName != null) {
+            image.setFileName(fileName);
+            imageRepository.save(image);
+        }
+        return new EditImageDetailsResponseDTO(fileName);
     }
-    return new ImageResponseDTO(
-        url, workspaceId, image.getFileName(), image.getBucketIdentifier(), null);
-  }
 
-  @Override
-  public Page<ImageResponseDTO> createDownloadPresignedUrlForImages(
-      Long userId, Long workspaceId, Boolean includeAnnotations, Pageable pageable) {
-    var images = getImagesIfWorkspaceUser(userId, workspaceId, pageable);
-    if (includeAnnotations != null && includeAnnotations) {
+    @Override
+    public ImageResponseDTO createDownloadPresignedUrlForImage(
+            Long userId, Long workspaceId, String bucketIdentifier, Boolean includeAnnotations) {
 
-      return images.map(
-          image -> {
-            var url =
-                s3Service.createGetPresignedUrl(
-                    image.getWorkspace().getBucketPrefix() + image.getBucketIdentifier());
+        var image = getImageIfWorkspaceUser(userId, workspaceId, bucketIdentifier);
+
+        // not n + 1...right?
+        var url =
+                s3Service.createGetPresignedUrl(image.getWorkspace().getBucketPrefix() + bucketIdentifier);
+
+        if (includeAnnotations != null && includeAnnotations) {
             return new ImageResponseDTO(
-                url,
-                workspaceId,
-                image.getFileName(),
-                image.getBucketIdentifier(),
-                image.getAnnotations());
-          });
+                    url,
+                    workspaceId,
+                    image.getFileName(),
+                    image.getBucketIdentifier());
+        }
+        return new ImageResponseDTO(
+                url, workspaceId, image.getFileName(), image.getBucketIdentifier());
     }
-    return images.map(
-        image -> {
-          var url =
-              s3Service.createGetPresignedUrl(
-                  image.getWorkspace().getBucketPrefix() + image.getBucketIdentifier());
-          return new ImageResponseDTO(
-              url, workspaceId, image.getFileName(), image.getBucketIdentifier(), null);
-        });
-  }
 
-  @Transactional
-  @Override
-  public void deleteImage(Long userId, Long workspaceId, String imageId) {
-    var image = getImageIfOwned(userId, workspaceId, imageId);
+    @Override
+    public Page<ImageResponseDTO> createDownloadPresignedUrlForImages(
+            Long userId, Long workspaceId, Boolean includeAnnotations, Pageable pageable) {
+        var images = getImagesIfWorkspaceUser(userId, workspaceId, pageable);
+        if (includeAnnotations != null && includeAnnotations) {
 
-    imageRepository.delete(image);
-    s3Service.deleteObject(image.getWorkspace().getBucketPrefix() + image.getBucketIdentifier());
-  }
-
-  // Make sure it is the owner making the changes
-  private Image getImageIfOwned(Long userId, Long workspaceId, String imageId) {
-    var image =
-        imageRepository.findById(imageId).orElseThrow(() -> new ImageNotFoundException(imageId));
-
-    var attachedWorkspace = image.getWorkspace();
-
-    if (!Objects.equals(attachedWorkspace.getId(), workspaceId)
-        || !Objects.equals(attachedWorkspace.getOwner().getId(), userId)) {
-      throw new NotAllowedException("Cannot remove owner from workspace");
+            return images.map(
+                    image -> {
+                        var url =
+                                s3Service.createGetPresignedUrl(
+                                        image.getWorkspace().getBucketPrefix() + image.getBucketIdentifier());
+                        return new ImageResponseDTO(
+                                url,
+                                workspaceId,
+                                image.getFileName(),
+                                image.getBucketIdentifier());
+                    });
+        }
+        return images.map(
+                image -> {
+                    var url =
+                            s3Service.createGetPresignedUrl(
+                                    image.getWorkspace().getBucketPrefix() + image.getBucketIdentifier());
+                    return new ImageResponseDTO(
+                            url, workspaceId, image.getFileName(), image.getBucketIdentifier());
+                });
     }
-    return image;
-  }
 
-  private Image getImageIfWorkspaceUser(Long userId, Long workspaceId, String imageId) {
-    var image =
-        imageRepository.findById(imageId).orElseThrow(() -> new ImageNotFoundException(imageId));
+    @Transactional
+    @Override
+    public void deleteImage(Long userId, Long workspaceId, String imageId) {
+        var image = getImageIfOwned(userId, workspaceId, imageId);
 
-    var attachedWorkspace = image.getWorkspace();
-
-    if (!Objects.equals(attachedWorkspace.getId(), workspaceId)
-        || attachedWorkspace.getWorkspaceUsers().stream()
-            .noneMatch(u -> Objects.equals(u.getUser().getId(), userId))) {
-      throw new NotAllowedException("Cannot remove owner from workspace");
+        imageRepository.delete(image);
+        s3Service.deleteObject(image.getWorkspace().getBucketPrefix() + image.getBucketIdentifier());
     }
-    return image;
-  }
 
-  private Page<Image> getImagesIfWorkspaceUser(Long userId, Long workspaceId, Pageable pageable) {
-    Workspace workspace =
-        workspaceRepository
-            .findByIdAndWorkspaceUsers_UserId(workspaceId, userId)
-            .orElseThrow(() -> new NotAllowedException("Cannot remove owner from workspace"));
+    // Make sure it is the owner making the changes
+    private Image getImageIfOwned(Long userId, Long workspaceId, String imageId) {
+        var image =
+                imageRepository.findById(imageId).orElseThrow(() -> new ImageNotFoundException(imageId));
 
-    return imageRepository.findImageByWorkspaceId(workspace.getId(), pageable);
-  }
+        var attachedWorkspace = image.getWorkspace();
+
+        if (!Objects.equals(attachedWorkspace.getId(), workspaceId)
+                || !Objects.equals(attachedWorkspace.getOwner().getId(), userId)) {
+            throw new NotAllowedException("Cannot remove owner from workspace");
+        }
+        return image;
+    }
+
+    private Image getImageIfWorkspaceUser(Long userId, Long workspaceId, String imageId) {
+        var image =
+                imageRepository.findById(imageId).orElseThrow(() -> new ImageNotFoundException(imageId));
+
+        var attachedWorkspace = image.getWorkspace();
+
+        if (!Objects.equals(attachedWorkspace.getId(), workspaceId)
+                || attachedWorkspace.getWorkspaceUsers().stream()
+                .noneMatch(u -> Objects.equals(u.getUser().getId(), userId))) {
+            throw new NotAllowedException("Cannot remove owner from workspace");
+        }
+        return image;
+    }
+
+    private Page<Image> getImagesIfWorkspaceUser(Long userId, Long workspaceId, Pageable pageable) {
+        Workspace workspace =
+                workspaceRepository
+                        .findByIdAndWorkspaceUsers_UserId(workspaceId, userId)
+                        .orElseThrow(() -> new NotAllowedException("Cannot remove owner from workspace"));
+
+        return imageRepository.findImageByWorkspaceId(workspace.getId(), pageable);
+    }
 }
