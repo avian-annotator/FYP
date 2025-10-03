b61d7a34cf9f757daabfbfa86c419e296c46f998
seba0205 <83637951+seba0205@users.noreply.github.com>
2025-10-03 16:22:58 +1000

    feat(AAS-0): tidy up

--- frontend/src/components/canvas/Tools/LabelTool.tsx
+++ frontend/src/components/canvas/Tools/LabelTool.tsx
@@ -1,15 +1,8 @@
-import { useAuth } from '@/auth'
 import { CanvasTool, CanvasToolProps } from '../Canvas'
 import Konva from 'konva'
 
 const LabelTool = (props: CanvasToolProps): CanvasTool => {
-  const userId = useAuth().userDetails?.id ?? 0
-
   const handleClick = (e: Konva.KonvaEventObject<MouseEvent>) => {
-    const user = props.canvasState.userState.toArray().find(u => u.userId === userId)
-    const element = props.canvasState.canvasElements
-      .toArray()
-      .find(el => el.id === user?.currentSelectionId)
     if (e.evt.button === 0) {
       if (e.target instanceof Konva.Shape) {
         const labelText = prompt('Enter label text:')
